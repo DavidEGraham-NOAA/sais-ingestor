2273ced96d9f12a149d40ad9d035c03f5ca33f76
David Graham <david.graham@noaa.gov>
2013-11-07 11:35:11 -1000

    Update qa_inspector.sql

--- qa_inspector.sql
+++ qa_inspector.sql
@@ -1,4 +1,6 @@
 --selects basic observation data from postgis database with kml point info for inspection
 SELECT ID, mmsi, reportdate, ST_AsKML(reportedpoint)
 FROM sais_reports
-where ID in (<ID LIST FROM R SCRIPT)
+WHERE ID IN (
+--<ID LIST FROM R SCRIPT>
+)
