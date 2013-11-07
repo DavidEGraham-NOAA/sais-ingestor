207af5dd5afa55aa4c1fe77c404863e908b0ccb0
David Graham <david.graham@noaa.gov>
2013-11-07 11:31:52 -1000

    Update obsparser.py

--- obsparser.py
+++ obsparser.py
@@ -2,7 +2,12 @@ import sys
 import csv
 import saisdb
 
-csv_file = 'C:\\2012Source\\IPython\\sais\\testset.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2008.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2009.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2010.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2011.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2012.txt'
+#csv_file = '/media/Seagate Backup Plus Drive/GIS/sais_IHS/combinedhistorydata 2013.txt'
 
 # open csv file
 with open(csv_file, 'rb') as csvfile:
@@ -24,5 +29,5 @@ with open(csv_file, 'rb') as csvfile:
     for row in reader:
         content = list(row[i] for i in included_cols)
         #print len(content)
-        saisdb.SaveObservation(content[0], content[1], content[2], content[3], content[4], content[5], content[6], content[7], content[8], content[9], content[10], content[11], content[12], content[13], content[14], content[15], content[16], content[17])
+        saisdb.SaveObservation(content[0], float(content[1]), float(content[2])*-1, content[3], content[4], content[5], content[6], content[7], content[8], content[9], content[10], content[11], content[12], content[13], content[14], content[15], content[16], content[17])
         #print content[0]
