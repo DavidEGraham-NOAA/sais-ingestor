d56762a747c456a0b554e5768feb9383da02c419
David Graham <david.graham@noaa.gov>
2013-12-11 16:33:51 -1000

    Update tracks_dev.sql

--- tracks_dev.sql
+++ tracks_dev.sql
@@ -34,6 +34,7 @@ DECLARE
   vessel RECORD;
   report RECORD;
   trackmember RECORD;
+  internaltrack RECORD;
   seqnum INT;
 BEGIN
   FOR vessel IN SELECT mmsi FROM vesseldetails GROUP BY mmsi LOOP
@@ -42,7 +43,11 @@ BEGIN
       FOR trackmember IN SELECT id, mmsi, reportdate FROM sais_reports WHERE mmsi = report.mmsi AND reportdate BETWEEN report.rptdate AND report.rptdate + interval '3 days' LOOP
         --for each one of these we also need to do the 3 day lookahead
         --insert here
-        RAISE NOTICE 'TRACK is %, %, % with trackid %', trackmember.id, quote_ident(trackmember.mmsi), trackmember.reportdate, seqnum;
+        --RAISE NOTICE 'TRACK is %, %, % with trackid %', trackmember.id, quote_ident(trackmember.mmsi), trackmember.reportdate, seqnum;
+          FOR internaltrack IN SELECT id, mmsi, reportdate FROM sais_reports WHERE mmsi = trackmember.mmsi AND reportdate BETWEEN trackmember.reportdate AND trackmember.reportdate + interval '3 days' LOOP
+            INSERT INTO sais_derived_tracks (fk_reportid, trackid) SELECT internaltrack.id, seqnum WHERE NOT EXISTS (SELECT * FROM sais_derived_tracks WHERE fk_reportid =  internaltrack.id);
+            RAISE NOTICE 'INTERNAL TRACK is %, %, % with trackid %', internaltrack.id, quote_ident(internaltrack.mmsi), internaltrack.reportdate, seqnum;
+          END LOOP;
       END LOOP;
       RAISE NOTICE '----------------------END TRACK-----------------------';
     END LOOP;
@@ -55,8 +60,16 @@ $$ LANGUAGE plpgsql;
 
 select * from get_vessel_tracks();
 
-select * from sais_reports where mmsi = '247184200' AND reportdate BETWEEN '2009-01-20 07:25:53'::timestamp AND '2009-01-20 07:25:53'::timestamp + interval '3 days'
-
-select * from seq_tracks;
-
 alter sequence seq_tracks restart with 1;
+
+select * from sais_derived_tracks
+
+select count(fk_reportid), trackid from sais_derived_tracks group by trackid order by count desc
+
+select * from sais_derived_tracks where trackid = 14304
+
+select * from sais_reports where id = 181054
+
+select * from sais_reports where mmsi = '477144240'
+
+SELECT * FROM vesseldetails WHERE updatedate IS NULL LIMIT 50;
