27d6033466caaaaaf6322a8b363a05c0889ee80c
David Graham <david.graham@noaa.gov>
2014-10-23 12:55:08 -1000

    Update web_sql.sql

--- web_sql.sql
+++ web_sql.sql
@@ -14,3 +14,32 @@ FROM ihs_observations INNER JOIN ihs_derived_tracks_dev
 ON ihs_obs_id = fk_ihs_obs_id
 GROUP BY trackid) AS sq
 where trackid = sq.trackid
+
+
+update mvw_track_details set startdate = sq.startdate, 
+enddate = sq.enddate, mmsi = sq.mmsi
+from (SELECT trackid, min(reportdate) as startdate, max(reportdate) as enddate,
+min(mmsi) AS mmsi
+FROM ihs_observations INNER JOIN ihs_derived_tracks_dev
+ON ihs_obs_id = fk_ihs_obs_id
+GROUP BY trackid) AS sq
+where fk_trackid = sq.trackid
+
+select * from mvw_track_details
+
+CREATE VIEW vw_track_details AS
+SELECT DISTINCT v.fk_trackid, 
+	to_char(startdate, 'MM/DD/YYYY') AS startdate, 
+	to_char(enddate, 'MM/DD/YYYY') AS enddate, 
+	vd.name,
+	vd.mmsi,
+	features_intersected, 
+	atbas_intersected, 
+	pointcount, 
+	v.length,
+	ta.invalid
+FROM mvw_track_details v
+	LEFT OUTER JOIN vesseldetails vd on vd.mmsi = v.mmsi
+	LEFT OUTER JOIN track_analysis ta on ta.fk_trackid = v.fk_trackid
+
+select * from vw_track_details
