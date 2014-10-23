931659b0d911a48538a029538cc0cc7357dd0902
David Graham <david.graham@noaa.gov>
2014-10-22 23:17:00 -1000

    Update web_sql.sql

--- web_sql.sql
+++ web_sql.sql
@@ -5,3 +5,12 @@ SELECT trackid, min(reportdate), max(reportdate)
 FROM ihs_observations INNER JOIN ihs_derived_tracks_dev
 ON ihs_obs_id = fk_ihs_obs_id
 GROUP BY trackid
+
+
+--start and end dates
+update mvw_track_details set startdate = sq.startdate, enddate = sq.enddate 
+from (SELECT trackid, min(reportdate) as startdate, max(reportdate) as enddate
+FROM ihs_observations INNER JOIN ihs_derived_tracks_dev
+ON ihs_obs_id = fk_ihs_obs_id
+GROUP BY trackid) AS sq
+where trackid = sq.trackid
